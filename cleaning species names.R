33c3a0ee095dc13bf84f599bfc4fd76ce0ae959e
wilcoxkr <wilcoxkr@gmail.com>
2019-04-22 11:29:12 -0600

    Import code to clean species names

---
+++ cleaning species names.R
@@ -0,0 +1,27 @@
+### Merge species names to get tnrs names
+### Authors: Kevin Wilcox (kevin.wilcox@uwyo.edu), Sally Koerner (sally.koerner@uncg.edu)
+###
+### Created April 9, 2019; Last updated: April 9, 2019
+
+### Set up workspace
+rm(list=ls())
+setwd("C:\\Users\\wilco\\Desktop\\Working groups\\Grazing\\analyses\\cleaning_data\\")
+library(tidyverse)
+
+### Import data
+relcov_full <- read.csv("ALL_Cleaned_Apr2019.csv")
+spname_key <- read.csv()
+
+
+### Merge TNRS names with names in GEx dataset
+relcov_tnrs_names <- relcov_full %>%
+  left_join(spname_key, by=) %>%
+  dplyr::select(-genus_species)
+
+### Select only last year of data
+relcov_last_year_only <- relcov_tnrs_names %>%
+  group_by(site) %>%
+  filter(year = max(year), exage == max(exage))
+
+
+
