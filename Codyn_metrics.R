ab7e3ac7225132040274a1de3c6e34620858c104
Meghan Avolio <meghanavolio@Meghans-MB.local>
2019-04-22 15:15:16 -0600

    composition difference, RAC_differences, and abudance_diff and how they all relate

--- Codyn_metrics.R
+++ Codyn_metrics.R
@@ -13,10 +13,10 @@ lsyear<-dat%>%
   mutate(site_block=paste(site, block, sep="##"))
 
 lsyear2<-lsyear%>%
-  filter(site_block!="California_Sedgwick_Lisque_A"&
-           site_block!="California_Sedgwick_Lisque_B"&
-           site_block!="California_Sedgwick_Lisque_G"&
-           site_block!="DesertLow_Alkali"&
+  filter(site_block!="California_Sedgwick_Lisque##A"&
+           site_block!="California_Sedgwick_Lisque##B"&
+           site_block!="California_Sedgwick_Lisque##G"&
+           site_block!="DesertLow##Alkali"&
            site!="Jornada"&
            site!="Junner Koeland"&
            site!="Konza")
@@ -44,12 +44,12 @@ lsyear3<-lsyear%>%
          site=="Junner Koeland"|
          site=="Konza")%>%
   mutate(site_block_plot=paste(site_block, plot, sep="##"))%>%
-  filter(site_block_plot!="Jornada_east_30"&
-           site_block_plot!="Jornada_west_18"&
-           site_block_plot!="Jornada_west_19"&
-           site_block_plot!="Jornada_west_20"&
-           site_block_plot!="Jornada_west_21"&
-           site_block_plot!="Jornada_west_22")
+  filter(site_block_plot!="Jornada##east##30"&
+           site_block_plot!="Jornada##west##18"&
+           site_block_plot!="Jornada##west##19"&
+           site_block_plot!="Jornada##west##20"&
+           site_block_plot!="Jornada##west##21"&
+           site_block_plot!="Jornada##west##22")
 
 gex_multdiff2<-data.frame()
 siteblockplot<-unique(lsyear3$site_block_plot)
@@ -76,7 +76,154 @@ ave_gexmultdiff2<-gex_multdiff2%>%
   summarize(composition_diff=mean(composition_diff))
 
 gex_multdiff_all<-gex_multdiff %>% 
-  bind_rows(ave_gexmultdiff2)
+  bind_rows(ave_gexmultdiff2)%>%
+  separate(site_block, into=c("site", "block"), sep="##")%>%
+  select(site, block, composition_diff)
+
+write.csv(gex_multdiff_all, "gex_multdiff_all.csv", row.names = F)
 
 gex_multdiff_ave<-gex_multdiff_all%>%
-  separate(site_block, into=c("site", "block", sep="##"))
+  separate(site_block, into=c("site", "block"), sep="##") %>% 
+  group_by(site) %>% 
+  summarize(composition_diff=mean(composition_diff))
+
+write.csv(gex_multdiff_ave, "gex_multdiff_ave.csv", row.names = F)
+
+##graphing compositional differences and CI
+gex_multdiff_CI<-gex_multdiff_all%>%
+  separate(site_block, into=c("site", "block"), sep="##") %>% 
+  group_by(site) %>% 
+  summarize_at(vars(composition_diff), funs(mean, sd, length)) %>% 
+  mutate(se=sd/sqrt(length),
+         ci=se*1.96)
+
+mean<-mean(gex_multdiff_CI$mean)
+sd<-sd(gex_multdiff_CI$mean)
+se<-sd/sqrt(252)
+ci<-se*1.96
+site<-"All Sites"
+
+all<- data.frame(site, mean, sd, se, ci)
+gex_multdiff_CI2<-gex_multdiff_CI%>%
+  bind_rows(all)
+
+
+ggplot(data=gex_multdiff_CI2, aes(x=as.factor(site), y=mean))+
+  geom_point()+
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci))+
+  coord_flip()+
+  geom_hline(yintercept = 0.25)
+
+###doing RAC differences
+
+gex_RACdiff<-data.frame()
+siteblock<-unique(lsyear2$site_block)
+
+for (i in 1:length(siteblock)){
+  
+  subset<-lsyear2%>%
+    filter(site_block==siteblock[i]) %>%
+    mutate(treatment=trt)
+  
+  out <- RAC_difference(subset, species.var="genus_species", abundance.var = "relcov", replicate.var = "trt", treatment.var = "treatment")
+  
+  out$site_block<-siteblock[i]
+  
+  gex_RACdiff<-rbind(gex_RACdiff, out)  
+}
+
+gex_RACdiff2<-data.frame()
+siteblockplot<-unique(lsyear3$site_block_plot)
+
+for (i in 1:length(siteblockplot)){
+  
+  subset<-lsyear3%>%
+    filter(site_block_plot==siteblockplot[i]) %>%
+    mutate(treatment=trt)
+  
+  out <- RAC_difference(subset, species.var="genus_species", abundance.var = "relcov", replicate.var = "trt", treatment.var = "treatment")
+  
+  un_site_block<-unique(subset$site_block)
+  un_plot<-unique(subset$plot)
+  
+  out$site_block<-un_site_block
+  out$plot<-un_plot
+  
+  gex_RACdiff2<-rbind(gex_RACdiff2, out)  
+}
+
+gexRACdiff2_ave<-gex_RACdiff2%>%
+  group_by(site_block, trt, trt2, treatment, treatment2)%>%
+  summarize_at(vars(richness_diff, evenness_diff, rank_diff, species_diff), funs(mean))
+
+gex_RACdiff_all<-gex_RACdiff %>% 
+  bind_rows(gexRACdiff2_ave)%>%
+  separate(site_block, into=c("site", "block"), sep="##")%>%
+  select(site, block, richness_diff, evenness_diff, rank_diff, species_diff)
+
+write.csv(gex_RACdiff_all, "gex_RACdiff_all.csv", row.names = F)
+
+gex_RACdiff_ave<-gex_RACdiff_all%>%
+  group_by(site) %>% 
+  summarize_at(vars(richness_diff, evenness_diff, rank_diff, species_diff), funs(mean))
+
+write.csv(gex_multdiff_ave, "gex_RACdiff_ave.csv", row.names = F)
+
+###doing abundance differences
+
+gex_abunddiff<-data.frame()
+siteblock<-unique(lsyear2$site_block)
+
+for (i in 1:length(siteblock)){
+  
+  subset<-lsyear2%>%
+    filter(site_block==siteblock[i]) %>%
+    mutate(treatment=trt)
+  
+  out <- abundance_difference(subset, species.var="genus_species", abundance.var = "relcov", replicate.var = "trt", treatment.var = "treatment")
+  
+  out$site_block<-siteblock[i]
+  
+  gex_abunddiff<-rbind(gex_abunddiff, out)  
+}
+
+gex_abunddiff2<-data.frame()
+siteblockplot<-unique(lsyear3$site_block_plot)
+
+for (i in 1:length(siteblockplot)){
+  
+  subset<-lsyear3%>%
+    filter(site_block_plot==siteblockplot[i]) %>%
+    mutate(treatment=trt)
+  
+  out <- abundance_difference(subset, species.var="genus_species", abundance.var = "relcov", replicate.var = "trt", treatment.var = "treatment")
+  
+  un_site_block<-unique(subset$site_block)
+  un_plot<-unique(subset$plot)
+  
+  out$site_block<-un_site_block
+  out$plot<-un_plot
+  
+  gex_abunddiff2<-rbind(gex_abunddiff2, out)  
+}
+
+gexabundiff2_ave<-gex_abunddiff2%>%
+  group_by(site_block, trt, trt2, treatment, treatment2, genus_species)%>%
+  summarize_at(vars(difference), funs(mean))
+
+gex_abunddiff_all<-gex_abunddiff %>% 
+  bind_rows(gexabundiff2_ave)%>%
+  separate(site_block, into=c("site", "block"), sep="##")%>%
+  select(site, block, genus_species, difference)
+
+write.csv(gex_abunddiff_all, "gex_abund_diff_all.csv", row.names = F)
+
+gex_abunddiff_ave<-gex_abunddiff_all%>%
+  group_by(site, genus_species) %>% 
+  summarize_at(vars(difference), funs(mean))
+
+write.csv(gex_abunddiff_ave, "gex_abund_diff_ave.csv", row.names = F)
+
+###doing a multiple regression, what is driving comp change
+comprac<-
+  
