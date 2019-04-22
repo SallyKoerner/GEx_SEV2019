813517d413074908438628921f00c058fe29f58f
wilcoxkr <wilcoxkr@gmail.com>
2019-04-22 12:02:41 -0600

    code to read in data

--- cleaning trait data.R
+++ cleaning trait data.R
@@ -3,3 +3,24 @@
 ### Authors: kevin wilcox (kevin.wilcox@uwyo.edu)
 ### created: April 22, 2019;  last updated: April 22, 2019; 
 
+file.choose()
+### Set up workspace
+rm(list=ls())
+setwd("C:\\Users\\wilco\\Desktop\\Working groups\\Grazing\\trait data\\")
+library(tidyverse)
+library(data.table)
+
+### REad in data
+try_data <- fread("try data_raw.txt", header = T, sep = "\t", dec = ".", quote = "", data.table = T)
+
+
+
+
+
+
+
+
+
+
+
+
