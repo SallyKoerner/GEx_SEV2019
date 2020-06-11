80a577d612e4f8e41e98edecb2671f9d9de6e1e9
Ava Hoffman <avamariehoffman@gmail.com>
2020-06-11 16:50:14 -0400

    Update permanova out to include any sites with >2 plots

--- Permanova_checks/bootstrap_permanova.R
+++ Permanova_checks/bootstrap_permanova.R
@@ -8,52 +8,59 @@ setwd("/Users/avahoffman/Dropbox/Research/Grazing_consortium/2020/GEx_SEV2019")
 
 dat <- read.csv("GEx_cleaned_11June2020.csv")
 
-dat2<-
-  dat%>%
-  mutate(drop=ifelse(
-    genus_species_use=="#N/A"|
-      genus_species_use=="Dead unidentified"|
-      genus_species_use=="Leaf.Litter"|
-      genus_species_use=="cactus__dead_", 1, 0))%>%
-  filter(drop!=1)
+dat2 <-
+  dat %>%
+  mutate(
+    drop = ifelse(
+      genus_species_use == "#N/A" |
+        genus_species_use == "Dead unidentified" |
+        genus_species_use == "Leaf.Litter" |
+        genus_species_use == "cactus__dead_",
+      1,
+      0
+    )
+  ) %>%
+  filter(drop != 1)
 
-exage<-
-  dat%>%
-  select(site, exage)%>%
-  unique()%>%
-  filter(exage>10)
+exage <-
+  dat %>%
+  select(site, exage) %>%
+  unique() %>%
+  filter(exage > 10)
 
-lsyear<-
-  dat%>%
-  group_by(site) %>% 
-  mutate(lyear=max(year),
-         mexage=max(exage))%>%
-  filter(year==lyear, mexage==exage)%>%
-  mutate(site_block=paste(site, block, sep="##"))
+lsyear <-
+  dat %>%
+  group_by(site) %>%
+  mutate(lyear = max(year),
+         mexage = max(exage)) %>%
+  filter(year == lyear, mexage == exage) %>%
+  mutate(site_block = paste(site, block, sep = "##"))
 
-#dropping probelmatic blocks and averaging over experiments where there are several blocks in a plot. 
+#dropping probelmatic blocks and averaging over experiments where there are several blocks in a plot.
 #Averaging up to a block for Konza, Junner Koeland, and Jornada sites.
 
-lsyear2<-
-  lsyear%>%
-  filter(site_block!="California_Sedgwick_Lisque##A"&
-           site_block!="California_Sedgwick_Lisque##B"&
-           site_block!="California_Sedgwick_Lisque##G"&
-           site_block!="DesertLow##Alkali")%>%
-  group_by(site, block, site_block, trt, exage, genus_species_use)%>%
-  summarize(relcov=mean(relcov))
+lsyear2 <-
+  lsyear %>%
+  filter(
+    site_block != "California_Sedgwick_Lisque##A" &
+      site_block != "California_Sedgwick_Lisque##B" &
+      site_block != "California_Sedgwick_Lisque##G" &
+      site_block != "DesertLow##Alkali"
+  ) %>%
+  group_by(site, block, site_block, trt, exage, genus_species_use) %>%
+  summarize(relcov = mean(relcov))
 
-numreps<-
-  lsyear%>%
-  select(site, block)%>%
-  unique()%>%
-  group_by(site)%>%
-  summarize(num=length(block))
+numreps <-
+  lsyear %>%
+  select(site, block) %>%
+  unique() %>%
+  group_by(site) %>%
+  summarize(num = length(block))
 
-lsyear2.2<-
-  lsyear2%>%
-  left_join(numreps)%>%
-  filter(num>3)
+lsyear2.2 <-
+  lsyear2 %>%
+  left_join(numreps) %>%
+  filter(num > 2)
 
 nmds_plot <-
   function(species, env, file_prefix = "out") {
@@ -65,12 +72,21 @@ nmds_plot <-
     scores2 <-
       cbind(env, scores) # binds the NMDS scores of year i to all years previously run
     
-    ggplot(scores2, 
-           aes(x = NMDS1, y = NMDS2, color = trt, shape = type)) +
+    ggplot(scores2,
+           aes(
+             x = NMDS1,
+             y = NMDS2,
+             color = trt,
+             shape = type
+           )) +
       geom_point(size = 5) +
-      scale_shape_manual(values = c(1,15))
+      scale_shape_manual(values = c(1, 15))
     
-    ggsave(paste("Permanova_checks/nmds/",file_prefix,".pdf",sep=""), height = 3, width = 4)                   
+    ggsave(
+      paste("Permanova_checks/nmds/", file_prefix, ".pdf", sep = ""),
+      height = 3,
+      width = 4
+    )
   }
 
 
@@ -88,7 +104,14 @@ run_grazing_bootstrap <-
       # Convert long spp dataset to wide
       wide <-
         subset %>%
-        select(site, site_block, exage, block, trt, num, genus_species_use, relcov) %>%
+        select(site,
+               site_block,
+               exage,
+               block,
+               trt,
+               num,
+               genus_species_use,
+               relcov) %>%
         spread(genus_species_use, relcov, fill = 0)
       
       # Species relative abundance matrix
@@ -145,14 +168,15 @@ run_grazing_bootstrap <-
         
         # Construct a data frame for the descriptions of the bootstrapped samples
         boot_env <-
-          data.frame(
-            trt = c(rep("G", replication_needed),
-                    rep("U", replication_needed)),
-            block = as.factor(c(rep(seq((max(env$block) + 1),
-                                        (max(env$block) + replication_needed)
-            ),
-            2))),
-            type = "boot")
+          data.frame(trt = c(
+            rep("G", replication_needed),
+            rep("U", replication_needed)
+          ),
+          block = as.factor(c(rep(seq((max(env$block) + 1),
+                                      (max(env$block) + replication_needed)
+          ),
+          2))),
+          type = "boot")
         
         # Bind bootstrapped and original samples
         env <- rbind(env, boot_env)
@@ -194,65 +218,95 @@ gex_permanova <- run_grazing_bootstrap()
 write.csv(gex_permanova, file = "Permanova_checks/permanova_bootstrap.csv")
 
 
-swoosh_plot <- 
-  function(gex_permanova){
+swoosh_plot <-
+  function(gex_permanova) {
     ###making comp diff shoowsh figure
-    comp<-read.csv("gex_multdiff_site_block.csv")
+    comp <- read.csv("gex_multdiff_site_block.csv")
     
     #getting CI for each site
-    gex_multdiff_CI<-comp%>%
-      group_by(site) %>% 
-      summarize_at(vars(composition_diff), funs(mean, sd, length)) %>% 
-      mutate(se=sd/sqrt(length),
-             ci=se*1.96)
+    gex_multdiff_CI <- comp %>%
+      group_by(site) %>%
+      summarize_at(vars(composition_diff), funs(mean, sd, length)) %>%
+      mutate(se = sd / sqrt(length),
+             ci = se * 1.96)
     
     #geting global mean comp diff
-    mean<-mean(gex_multdiff_CI$mean)
-    sd<-sd(gex_multdiff_CI$mean)
-    se<-sd/sqrt(252)
-    ci<-se*1.96
-    site<-"All Sites"
+    mean <- mean(gex_multdiff_CI$mean)
+    sd <- sd(gex_multdiff_CI$mean)
+    se <- sd / sqrt(252)
+    ci <- se * 1.96
+    site <- "All Sites"
     
-    all<- data.frame(site, mean, sd, se, ci)
+    all <- data.frame(site, mean, sd, se, ci)
     
-    gex_multdiff_CI <- gex_multdiff_CI[order(gex_multdiff_CI$mean), ]  # sort
-    gex_multdiff_CI2<-gex_multdiff_CI%>%
-      bind_rows(all)%>%
-      mutate(colortrt=ifelse(site=="All Sites", 1,0))
-    gex_multdiff_CI2$site2 <- factor(gex_multdiff_CI2$site, levels = gex_multdiff_CI2$site)
+    gex_multdiff_CI <-
+      gex_multdiff_CI[order(gex_multdiff_CI$mean),]  # sort
+    gex_multdiff_CI2 <- gex_multdiff_CI %>%
+      bind_rows(all) %>%
+      mutate(colortrt = ifelse(site == "All Sites", 1, 0))
+    gex_multdiff_CI2$site2 <-
+      factor(gex_multdiff_CI2$site, levels = gex_multdiff_CI2$site)
     
     
-    ggplot(data=gex_multdiff_CI2, aes(x=site2, y=mean, color=as.factor(colortrt)))+
-      geom_point()+
-      scale_color_manual(values=c("black", "red"))+
-      geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci))+
-      coord_flip()+
-      xlab("Site")+
-      ylab("Compositional Difference")+
+    ggplot(data = gex_multdiff_CI2,
+           aes(
+             x = site2,
+             y = mean,
+             color = as.factor(colortrt)
+           )) +
+      geom_point() +
+      scale_color_manual(values = c("black", "red")) +
+      geom_errorbar(aes(ymin = mean - ci, ymax = mean + ci)) +
+      coord_flip() +
+      xlab("Site") +
+      ylab("Compositional Difference") +
       #scale_y_continuous(limits = c(0,1))+
-      theme(axis.text.y=element_blank(),
-            axis.ticks.y = element_blank(),
-            legend.position = "none")
+      theme(
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none"
+      )
     
     ###making comp diff sig figure based on permanovas
     
     ####flagging sig values
     permsig <- gex_permanova %>%
-      mutate(sig = ifelse(perm_Pvalue<.1, 1, 0))
+      mutate(sig = ifelse(perm_Pvalue < .05, 1, 0))
     
-    toplot<-gex_multdiff_CI%>%
+    toplot <- gex_multdiff_CI %>%
       right_join(permsig)
     
-    toplot$site2 <- factor(toplot$site, levels = toplot$site[order(toplot$mean)])
+    toplot$site2 <-
+      factor(toplot$site, levels = toplot$site[order(toplot$mean)])
     
-    ggplot(data=toplot, aes(x=site2, y=mean, color=as.factor(sig)))+
-      geom_point()+
-      scale_color_manual(values=c("black", "red"), name="Sig. Permanova", labels=c("No", "Yes"))+
-      geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci))+
-      coord_flip()+
-      xlab("Site")+
-      ylab("Compositional Difference")+
+    ggplot(data = toplot, aes(
+      x = site2,
+      y = mean,
+      color = as.factor(sig),
+      shape = as.factor(boot_n)
+    )) +
+      theme_classic() +
+      geom_point() +
+      scale_color_manual(
+        values = c("black", "red"),
+        name = "Permanova p < 0.05",
+        labels = c("No", "Yes")
+      ) +
+      scale_shape_manual(
+        name = "Num. bootstrapped plots",
+        values = c(1,2,3,4)
+      ) +
+      geom_errorbar(aes(ymin = mean - ci, ymax = mean + ci)) +
+      coord_flip() +
+      xlab("Site") +
+      ylab("Compositional Difference") +
       #scale_y_continuous(limits = c(0,1))+
-      theme(axis.text.y=element_blank(),
-            axis.ticks.y = element_blank())
+      theme(axis.text.y = element_blank(),
+            axis.ticks.y = element_blank()) +
+      theme(legend.position = c(0.95,0.05),
+            legend.justification = c(1,0))
+    
+    ggsave(file = "Permanova_checks/swooshplot_bootstrap.pdf",
+           height = 6,
+           width = 8)
   }
