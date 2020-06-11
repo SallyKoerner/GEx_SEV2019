e326520b335ba1b81ba5d1ec1d1c5221ca429a0b
SallyKoerner <koerner@usf.edu>
2020-06-11 16:42:00 -0400

    Updating, with new files... major overhaul, first half

--- merge_community_diff_metrics.R
+++ merge_community_diff_metrics.R
@@ -5,16 +5,66 @@
 ##  Date created: April 25, 2019
 ################################################################################
 
+###########Complete rewrite by Sally Koerner and Meghan Avolio ######################
+###########Rewrite June 11, 2020
 library(tidyverse)
+library(codyn)
 
 #kim's laptop
 setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\GEx working groups\\SEV 2019\\data')
+setwd("~/Dropbox/GEx_VirtualWorkshop_June2020")
 
+#Calculate Simposons LRR
+dat<-read.csv('GEx_cleaned_11June2020.csv')
+dat2<-dat%>%
+  mutate(drop=ifelse(genus_species_use=="#N/A"|genus_species_use=="Dead unidentified"|genus_species_use=="Leaf.Litter"|genus_species_use=="cactus__dead_", 1, 0))%>%
+  filter(drop!=1)
+lsyear<-dat2%>%
+  group_by(site) %>% 
+  mutate(lyear=max(year),
+         mexage=max(exage))%>%
+  filter(year==lyear, mexage==exage) %>% 
+  mutate(site_block=paste(site, block, sep="##"))
+  
+
+#dropping probelmatic blocks and averaging over experiments where there are several blocks in a plot. 
+#Averaging up to a block for Konza, Junner Koeland, and Jornada sites.
+
+lsyear2<-lsyear%>%
+  filter(site_block!="California_Sedgwick_Lisque##A"&
+           site_block!="California_Sedgwick_Lisque##B"&
+           site_block!="California_Sedgwick_Lisque##G"&
+           site_block!="DesertLow##Alkali")%>%
+  group_by(site, block, exage, year, trt, genus_species_use, site_block)%>%
+  summarize(relcov=mean(relcov)) %>% 
+  mutate(site_block_trt=paste(site_block, trt, sep="##"))
+
+SimpD<-community_diversity(lsyear2, replicate.var="site_block_trt", abundance.var="relcov", metric="InverseSimpson") %>% 
+  mutate(SimpD=1/InverseSimpson) %>% 
+  separate(site_block_trt, into=c("site", "block", "trt"), sep="##") %>% 
+  group_by(site, trt) %>% 
+  summarise(SimpD=mean(SimpD)) %>% 
+  spread(trt, SimpD) %>% 
+  mutate(SimpD_rr=(log(G/U))) %>% 
+  select(-G, -U)
+  
+RACdiff <- read.csv('gex_RACdiff_site_ave.csv') #RAC differences
+compDiff <- read.csv('gex_multdiff_site_ave.csv') #composition difference
+domIDdiff <- read.csv('Diff_BP_Dom_allyrs_11June2020.csv') %>%  #difference in identity of dominant spp
+  group_by(site) %>% 
+  mutate(lyear=max(year),
+         mexage=max(exage))%>%
+  filter(year==lyear, mexage==exage) %>% 
+  summarise(bp_rr=mean(bp_rr), diff_sp=mean(diff_sp))
+
+
+### where we stopped on 11 June 2020
+
+
+#####Kim's Old Code satrts here
 ###import data
 domDiff <- read.csv('OnlineVersion_withSimpD_Apr2018.csv')%>%select(-X) #dominance difference, with a bunch of site level covariates
-compDiff <- read.csv('gex_multdiff_ave.csv') #composition difference
-RACdiff <- read.csv('gex_RACdiff_ave_lnRR.csv') #RAC differences
-domIDdiff <- read.csv('Dom_SameDiff_bySite_SEV_April2019.csv')%>%select(-X)%>%rename(IDdiff=num) #difference in identity of dominant spp
+
 codomDiff <- read.csv('GEx_codominane_04242019.csv')%>% #codominance imported, need to calculate diff
   select(site, block, plot, trt, num_codominants)%>%
   unique()%>%
