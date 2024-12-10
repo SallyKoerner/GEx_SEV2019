286d9b809f7431bd72cb0b46f3060f667d47874c
Sally Koerner <sekoerne@uncg.edu>
2024-12-10 15:59:54 -0500

    Trying to add in a swoosh graph of rich)diff since thats what people normally look at.

--- composition/Swoosh figure composition paper.R
+++ composition/Swoosh figure composition paper.R
@@ -186,3 +186,70 @@ examples<-grid.arrange(high, mid, low, ncol=1)
 figure1<-grid.arrange(shoosh, examples, ncol=2)
 
 ggsave("Figure1_Dec2024.jpg", figure1)
+
+
+#NOT sure I like this last figure here. I think a histogram would actually be more helpful showing the mean and range with a t-test from 0? Maybe with the control change as a mark (and se)
+
+
+
+
+
+
+#####Make Same Rich_Diff Figure
+rac<-read.csv("gex_racdiff_site_ave.csv")
+#getting CI for each site
+gex_richdiff_CI<-rac%>%
+  group_by(site) %>% 
+  summarize_at(vars(richness_diff), funs(mean, sd, length)) %>% 
+  mutate(se=sd/sqrt(length),
+         ci=se*1.96)
+
+#geting global mean comp diff
+mean<-mean(gex_richdiff_CI$mean)
+sd<-sd(gex_richdiff_CI$mean)
+se<-sd/sqrt(252)
+ci<-se*1.96
+site<-"All Sites"
+#not including green dot.
+all<- data.frame(site, mean, sd, se, ci)
+
+gex_richdiff_CI <- gex_richdiff_CI[order(gex_richdiff_CI$mean), ]  # sort
+
+gex_richdiff_CI2<-gex_richdiff_CI%>%
+  mutate(colortrt=ifelse(site=="AUS_Berry", 2, ifelse(site=="CPER", 3, ifelse(site=="Kruger_Satara", 4, 0))))%>%
+  mutate(size=ifelse(site=="AUS_Berry"|site=="CPER"|site=="Kruger_Satara", 0.5, 0))
+
+gex_richdiff_CI2$site2 <- factor(gex_richdiff_CI2$site, levels = gex_richdiff_CI2$site)
+
+highlight<-gex_richdiff_CI2%>%
+  filter(site=="AUS_Berry"|site=="CPER"|site=="Kruger_Satara")
+
+shoosh<-
+  ggplot(data=gex_richdiff_CI2, aes(x=site2, y=mean, color=as.factor(colortrt)))+
+  geom_rect(aes(xmin = "Canada_UG3.LDB", xmax = "Argentina_S10", ymin=(-0.02812496-0.02522766), ymax=(-0.02812496+0.02522766)), fill="grey", alpha=0.25, color=NA)+
+  geom_hline(yintercept=0, linetype="solid", color="red")+
+  geom_hline(yintercept=-0.02812496, linetype="dotted")+
+  geom_point(aes(size=as.factor(size)))+
+  scale_size_manual(values=c(1, 3.5))+
+  scale_color_manual(values=c("black", "dodgerblue", "red", "orange"))+
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se))+
+  geom_point(data=highlight, aes(x=site2, y= mean, color=as.factor(colortrt)), size=3.5)+
+  coord_flip()+
+  xlab("Site")+
+  ylab("Richness Difference")+
+  scale_y_continuous(limits = c(-1,1))+
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+mean<-mean(gex_richdiff_CI$mean)
+sd<-sd(gex_richdiff_CI$mean)
+se<-sd/sqrt(252)
+ci<-se*1.96
+site<-"All Sites"
+#not including green dot.
+all<- data.frame(site, mean, sd, se, ci)
+
+ggsave("Figure_RichDiffSwoosh_Dec2024.jpg", shoosh)
